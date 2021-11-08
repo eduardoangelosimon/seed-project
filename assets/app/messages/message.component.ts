459b696e76c34704c7ab58f38bb03857f3cb0a56
Eduardo Angelo Simon <eduardosimon.trabalho@gmail.com>
2021-11-08 00:32:50 -0300

    parada nos slides pg. 29

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,37 +1,42 @@
-import { EventEmitter, Input, Output } from "@angular/core";
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { AppComponent } from "../app.component";
 import { Message } from "./message.model";
 
 @Component({
-  selector: 'app-message',
-  templateUrl: './message.component.html',
-  styles: [`
-    .author {
+    selector: 'app-message',
+    templateUrl: './message.component.html',
+    styles:[`
+    .author{
         display: inline-block;
         font-style: italic;
         font-size: 12px;
         width: 80%;
     }
-    .config {
+    .autor{
+        display: inline-block;
+        font-style: normal;
+        font-size: 12px;
+        width: 80%;
+    }
+    .config{
         display: inline-block;
         text-align: right;
         font-size: 12px;
         width: 19%;
     }
-  `]
+`]
 })
-
 export class MessageComponent {
-  @Input() messageVarClasse : Message = new Message("","");
-  @Input('inputMessage') messageVarClasseAlias : Message = new Message("","");
-  
-  @Output() editClicked_MessageMetodoClasse = new EventEmitter<string>();
-  @Output('outputMessage') editClicked_MessageMetodoClasseAlias = new EventEmitter<string>();
-  
+    color ='yellow';
+    @Input() messageVarClasse : Message = new Message("","");
+    @Input('inputMessage') messageVarClasseAlias : Message = new Message("","");
 
-  onEdit(){
-    // alert('Táh funcionando...');
-    this.editClicked_MessageMetodoClasse.emit("Texto veio de message (child) para o app (pai)");
-    this.editClicked_MessageMetodoClasseAlias.emit("Texto veio de message (child) para o app (pai) - Alias");
-  }
+    @Output() editClicked_MessageMetodoClasse = new EventEmitter<string>();
+    @Output('outputMessage') editClicked_MessageMetodoClasseAlias = new EventEmitter<string>();
+
+    onEdit(){
+    // alert('Ta funfando')
+        this.editClicked_MessageMetodoClasse.emit("Texto veio de message (child) para o app (pai)");
+        this.editClicked_MessageMetodoClasseAlias.emit("Texto veio de message (child) para o app (pai) - Alias");
+    }
 }
